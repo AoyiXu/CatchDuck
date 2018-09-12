4ae4758e9f6a415ecacd0f905485e0fb1866504c
XLQ <xlq_zhong@163.com>
2018-09-12 21:55:19 +0800

    catchduck init

---
+++ catchduck/src/main/java/com/xlq/game/catchduck/App.java
@@ -0,0 +1,13 @@
+package com.xlq.game.catchduck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
